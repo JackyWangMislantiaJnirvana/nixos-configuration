577cb9dfbbd6d3be4fd29670ee11cb38603e1358
Jacky Wang <wmjwld@live.cn>
2021-08-03 17:26:47 +0800

    remove v2ray from environment.systemPackages since it is redundant

--- configuration.nix
+++ configuration.nix
@@ -38,7 +38,6 @@
     vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     wget
     firefox
-    v2ray
   ];
 
   # Some programs need SUID wrappers, can be configured further or are
