3cbdc72bd4190240ac87cc4a49220ae5f9ac5ef2
Jacky Wang <wmjwld@live.cn>
2021-08-27 23:35:27 +0800

    let home-manager takeover packages installed using nix-env

--- homemanager/home.nix
+++ homemanager/home.nix
@@ -1,21 +1,32 @@
 { config, pkgs, ... }:
 
 {
-  # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
 
-  # Home Manager needs a bit of information about you and the
-  # paths it should manage.
   home.username = "jacky";
   home.homeDirectory = "/home/jacky";
 
-  # This value determines the Home Manager release that your
-  # configuration is compatible with. This helps avoid breakage
-  # when a new Home Manager release introduces backwards
-  # incompatible changes.
-  #
-  # You can update Home Manager without changing this value. See
-  # the Home Manager release notes for a list of state version
-  # changes in each release.
+  # Packages
+  nixpkgs.config.allowUnfree = true;
+  home.packages = with pkgs; [
+    alacritty
+    bat
+    gh
+    neofetch
+    neovim
+    okular
+    tldr
+    typora
+    vscode
+    zoom
+    zsh
+  ];
+
+  programs.git = {
+    enable = true;
+    userName = "Jacky Wang";
+    userEmail = "wmjwld@live.cn";
+  };
+
   home.stateVersion = "21.11";
 }
