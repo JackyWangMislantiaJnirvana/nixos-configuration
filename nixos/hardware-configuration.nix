d4dc03dc00abeeecb33502134d28f0a21e2ff5a9
Jacky Wang <wmjwld@live.cn>
2021-10-18 12:22:28 +0800

    try to move /boot into rootfs

--- nixos/hardware-configuration.nix
+++ nixos/hardware-configuration.nix
@@ -5,30 +5,21 @@
 
 {
   imports =
-    [
-      (modulesPath + "/installer/scan/not-detected.nix")
+    [ (modulesPath + "/installer/scan/not-detected.nix")
     ];
 
-  boot.initrd.availableKernelModules = [ "xhci_pci" "ahci" "usb_storage" "sd_mod" ];
-  boot.initrd.kernelModules = [];
-  boot.kernelModules = [];
-  boot.extraModulePackages = [];
+  boot.initrd.availableKernelModules = [ "xhci_pci" "ahci" "usbhid" "usb_storage" "sd_mod" ];
+  boot.initrd.kernelModules = [ ];
+  boot.kernelModules = [ "kvm-intel" ];
+  boot.extraModulePackages = [ ];
 
   fileSystems."/" =
-    {
-      device = "/dev/disk/by-uuid/e3724f8c-ddd0-4595-b342-eff7c0d36212";
+    { device = "/dev/disk/by-uuid/e3724f8c-ddd0-4595-b342-eff7c0d36212";
       fsType = "ext4";
     };
 
-  fileSystems."/boot" =
-    {
-      device = "/dev/disk/by-uuid/1E1F-3C49";
-      fsType = "vfat";
-    };
-
   swapDevices =
-    [
-      { device = "/dev/disk/by-uuid/d9307719-caec-4316-9dac-5e9820d4be40"; }
+    [ { device = "/dev/disk/by-uuid/d9307719-caec-4316-9dac-5e9820d4be40"; }
     ];
 
   powerManagement.cpuFreqGovernor = lib.mkDefault "powersave";
